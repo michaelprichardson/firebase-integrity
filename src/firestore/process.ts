7360bfa0855ddc2924c4797c345b239314ff46e0
Michael Richardson <michael@richardsons.co.za>
2021-05-12 20:40:14 +0100

    Fix actions

--- src/firestore/process.ts
+++ src/firestore/process.ts
@@ -1,33 +1,38 @@
-import { Change, EventContext, firestore, logger } from 'firebase-functions';
+import {
+  Change,
+  EventContext,
+  firestore as firebaseFunctions,
+  logger
+} from 'firebase-functions';
 import { Config } from '../common/config';
 import { FirestoreIntegrityConfig } from '../common/rules';
 
 export const processFirestoreTrigger = async (
   firebaseConfig: Config,
   integrityConfig: FirestoreIntegrityConfig,
-  snapshot: firestore.QueryDocumentSnapshot | Change<firestore.QueryDocumentSnapshot>,
+  snapshot: firebaseFunctions.QueryDocumentSnapshot | Change<firebaseFunctions.QueryDocumentSnapshot>,
   context: EventContext,
 ) => {
   const { firestore, database, storage } = integrityConfig.rules;
 
   // Check if there are any rules for Firestore
   if (firestore.length) {
-
+    logger.info('TODO: Need to be implemented');
   } else {
-    logger.warn('No rules for Firestore')
+    logger.warn('No rules for Firestore');
   }
 
   // Check if there are any rules for Database
   if (database.length) {
-
+    logger.info('TODO: Need to be implemented');
   } else {
-    logger.warn('No rules for Database')
+    logger.warn('No rules for Database');
   }
 
   // Check if there are any rules for Storage
   if (storage.length) {
-
+    logger.info('TODO: Need to be implemented');
   } else {
-    logger.warn('No rules for Storage')
+    logger.warn('No rules for Storage');
   }
 }
